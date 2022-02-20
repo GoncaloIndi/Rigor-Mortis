ae55ca14b5d028f7ad68c62108fd69b88ab0c584
GoncaloIndi <78227900+GoncaloIndi@users.noreply.github.com>
2022-02-20 22:15:58 +0000

    Camera Fully Implemented and more models

---
+++ Assets/Scripts/Camera/IncreaseFOV.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseFOV : MonoBehaviour
+{
+    [SerializeField] private float yOffset; //Determines how much the camera will go up
+
+    public FollowPlayer CameraFollowScript;
+
+    private void Awake()
+    {
+        CameraFollowScript = FindObjectOfType<FollowPlayer>().GetComponent<FollowPlayer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CameraFollowScript.CameraOffset += new Vector3(0, yOffset, 0);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        CameraFollowScript.CameraOffset -= new Vector3(0, yOffset, 0);
+    }
+}

8c3b90211f90a3cfa07c56d2e2273adc3059b99e
GoncaloIndi <78227900+GoncaloIndi@users.noreply.github.com>
2022-05-13 13:47:52 +0100

    CableLightPrototype

---
+++ Assets/Scripts/Ambient/Vents/LooseCableLights.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class LooseCableLights : MonoBehaviour
+{
+    [SerializeField] private Light cableLight;
+    [SerializeField] private float[] lightTime;
+    [SerializeField] private float lightChance = 69;
+
+    private void Awake()
+    {
+        cableLight = GetComponent<Light>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(CableLights());
+        
+    }
+
+    private IEnumerator CableLights()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(lightTime[0]);
+            StartCoroutine(EnableLights());
+            yield return new WaitForSeconds(lightTime[1]);
+            StartCoroutine(EnableLights());
+            yield return new WaitForSeconds(lightTime[2]);
+            StartCoroutine(EnableLights());
+            yield return new WaitForSeconds(lightTime[3]);
+            StartCoroutine(EnableLights());
+            yield return new WaitForSeconds(lightTime[4]);
+        }
+    }
+
+    private IEnumerator EnableLights()
+    {
+        float rng = Random.Range(0, 100);
+        if (rng > lightChance) yield break;
+
+        cableLight.enabled = true;
+        yield return new WaitForSeconds(.1f);
+        cableLight.enabled = false;
+    }
+}

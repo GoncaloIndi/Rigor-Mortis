2830c9009b624a91bc8b1f2b4715da28e9daaf0e
GoncaloIndi <78227900+GoncaloIndi@users.noreply.github.com>
2022-05-31 13:40:49 +0100

    Fmod&CutsceneToggle

--- Assets/Scripts/UI/StartingCutscene.cs
+++ Assets/Scripts/UI/StartingCutscene.cs
@@ -10,24 +10,33 @@ public class StartingCutscene : MonoBehaviour
 
     private bool isOnLastTextDisplay = false;
 
+    [SerializeField] private bool shouldPlayCutscene = true;
     [SerializeField] private Animator DarkenerAnim;
-    private static readonly int Ligthen = Animator.StringToHash("Ligthen");
     private static readonly int Lighten = Animator.StringToHash("Lighten");
 
     public PlayerStats PlayerStatsScript;
+    private static readonly int ForceDefault = Animator.StringToHash("ForceDefault");
 
     private void Awake()
     {
-        cutsceneText = this.gameObject.GetComponent<Text>();
-        PlayerStatsScript = FindObjectOfType<PlayerStats>().GetComponent<PlayerStats>();
-        PlayerStatsScript.CanMove = false;
+        if (!shouldPlayCutscene)
+        {
+            this.gameObject.SetActive(false);
+            DarkenerAnim.SetTrigger(ForceDefault);
+        }
+        else
+        {
+            cutsceneText = this.gameObject.GetComponent<Text>();
+            PlayerStatsScript = FindObjectOfType<PlayerStats>().GetComponent<PlayerStats>();
+            PlayerStatsScript.CanMove = false;
+        }
     }
 
     private void NextDisplay() //Both functions called by animation FadeText
     {
         if (isOnLastTextDisplay)
         {
-            Destroy(this.gameObject);
+            this.gameObject.SetActive(false);  
         }
         else
         {

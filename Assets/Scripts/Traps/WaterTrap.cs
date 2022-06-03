7bdbc1b609ca449c500c81981fb52c620d65979b
GoncaloIndi <78227900+GoncaloIndi@users.noreply.github.com>
2022-06-03 02:11:01 +0100

    Polishing

--- Assets/Scripts/Traps/WaterTrap.cs
+++ Assets/Scripts/Traps/WaterTrap.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using XInputDotNetPure;
@@ -13,6 +14,7 @@ public class WaterTrap : MonoBehaviour
     GamePadState prevState;
 
     private bool isGettingEletrified = false;
+    private bool hasKilledRat = false;
 
     private void Awake()
     {
@@ -39,11 +41,33 @@ public class WaterTrap : MonoBehaviour
                 EnemyCombat enemy = other.gameObject.GetComponent<EnemyCombat>();
                 if (enemy != null)
                 {
+                    hasKilledRat = true;
                     StartCoroutine(enemy.ElectrifyEnemy());
                 }
             }
         }
        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy") && !hasKilledRat)
+        {
+            RatStateManager ratState = other.gameObject.GetComponent<RatStateManager>();
+            if (ratState.CurrentTarget == null)
+            {
+                ratState.ReturnToOrigin = true;
+            }
+            else
+            {
+                EnemyCombat enemy = other.gameObject.GetComponent<EnemyCombat>();
+                if (enemy != null)
+                {
+                    hasKilledRat = true;
+                    StartCoroutine(enemy.ElectrifyEnemy());
+                }
+            }
+        }
     }
 
     private void OnTriggerExit(Collider other)
